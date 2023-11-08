5ee4e4d8809ba9acc8204568df84f3b1aaea7b9e
Long Pham <philong.bmt@gmail.com>
2023-11-08 17:33:58 +0700

    add `SwiftUI` host app

---
+++ ios/SwiftUI Host App/SwiftUI_Host_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Host_AppApp.swift
+//  SwiftUI Host App
+//
+//  Created by Long Pham on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Host_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
