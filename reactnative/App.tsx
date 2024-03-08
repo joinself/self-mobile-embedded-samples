28f779444abf509e7872cb7c7d6838feac4226f0
Vu Do <vu@joinself.com>
2024-03-08 16:32:45 +0700

    update readme

--- reactnative/App.tsx
+++ reactnative/App.tsx
@@ -127,13 +127,13 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-
-          <Section title="Self">
+          
+          <View style={styles.container}>
             <Text>
               SelfId: {selfId}
             </Text>
@@ -153,17 +153,7 @@ function App(): React.JSX.Element {
                   
               }}
             />           
-          </Section>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>          
+          </View>                  
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -171,6 +161,12 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    padding: 20,
+    margin: 20,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
