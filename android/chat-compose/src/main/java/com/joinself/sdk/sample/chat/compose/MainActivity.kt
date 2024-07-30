8f1446a054bd388d9451cfea0d7e06b26b63b9a7
Vu Do <vu@joinself.com>
2024-07-30 13:50:22 +0700

    update libraries

--- android/chat-compose/src/main/java/com/joinself/sdk/sample/chat/compose/MainActivity.kt
+++ android/chat-compose/src/main/java/com/joinself/sdk/sample/chat/compose/MainActivity.kt
@@ -67,6 +67,7 @@ import com.joinself.sdk.ui.addEmailRoute
 import com.joinself.sdk.ui.addLivenessCheckRoute
 import com.joinself.sdk.ui.addOnboardingRoute
 import com.joinself.sdk.ui.addPassportVerificationRoute
+import com.joinself.sdk.ui.addPhoneRoute
 import com.joinself.sdk.ui.addReadPassportRoute
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -176,9 +177,6 @@ class MainActivity : ComponentActivity() {
                                 },
                                 onNavigateToPassport = {
                                     navController.navigate("passportRoute")
-                                },
-                                onNavigateToReadPassport = {
-                                    navController.navigate("readPassportRoute")
                                 },
                                 onNavigateToOnboarding = {
                                     navController.navigate("onboardingRoute")
@@ -328,15 +326,9 @@ class MainActivity : ComponentActivity() {
                         showPassportDialog = "Failed"
                     }
                 }
-                addReadPassportRoute(navController, route = "readPassportRoute", account, this@MainActivity) { exception ->
-                    if (exception == null) {
-                        showPassportDialog = "Success"
-                    } else {
-                        showPassportDialog = "Failed"
-                    }
-                }
                 addOnboardingRoute(navController, route = "onboardingRoute")
                 addEmailRoute(navController, route = "emailRoute")
+                addPhoneRoute(navController, route = "phoneRoute")
             }
         }
     }
@@ -385,7 +377,6 @@ fun MainView(
     onNavigateToLivenessCheck: () -> Unit,
     onNavigateToMessaging: () -> Unit,
     onNavigateToPassport: () -> Unit,
-    onNavigateToReadPassport: () -> Unit,
     onNavigateToOnboarding: () -> Unit,
     onNavigateToEmail: () -> Unit,
     onExportBackup: () -> Unit,
@@ -455,20 +446,15 @@ fun MainView(
             Text(text = "Passport Verification")
         }
         Button(onClick = {
-            onNavigateToReadPassport.invoke()
+            onNavigateToOnboarding.invoke()
         }, enabled = !selfId.isNullOrEmpty()) {
-            Text(text = "Read Passport")
+            Text(text = "Onboarding")
         }
-//        Button(onClick = {
-//            onNavigateToOnboarding.invoke()
-//        }, enabled = !selfId.isNullOrEmpty()) {
-//            Text(text = "Onboarding")
-//        }
-//        Button(onClick = {
-//            onNavigateToEmail.invoke()
-//        }, enabled = !selfId.isNullOrEmpty()) {
-//            Text(text = "Email")
-//        }
+        Button(onClick = {
+            onNavigateToEmail.invoke()
+        }, enabled = !selfId.isNullOrEmpty()) {
+            Text(text = "Email")
+        }
         Button(onClick = {
             onShareLog.invoke()
         }, enabled = !selfId.isNullOrEmpty()) {
